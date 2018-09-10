61fd0eaf0031cb06b2e06c7c55f4a19b4f325775
tanthanadon <u5988073@cluster.local>
2018-09-10 10:35:43 +0700

    Test

--- Exercise/Q1.c
+++ Exercise/Q1.c
@@ -2,18 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-int A[100];
+	int A[100];
+	int i = 0;
+	srand(1234);
+	/* Set random seed */
 
-srand(1234);
+	for (i=0; i < 100; i++){
 
-/* Set random seed */
+  		A[i] = (rand()%1000)*10;
+ 		printf("%d\n", A[i]);
+	}
 
-for (i=0; i < 100; i++){
-
-  A[i] = (rand()%1000)*10;
-  printf("%d/n", A[i]);
-}
-
-/* Set each element randomly to 0-999 */
+	/* Set each element randomly to 0-999 */
 
 }
